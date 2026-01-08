87c64f0d1e647cf8e1c0e1a041d697f53aa594c7
tmanning90 <tmanning@carolinaday.org>
2026-01-08 14:25:31 +0000

    HW

--- Tuesday.py
+++ Tuesday.py
@@ -1,3 +1,16 @@
 print()
-print(Happy New Year)
+print("Happy New Year")
 print()
+
+schoolName = "CDS"
+print("Welcome to " + schoolName + ",")
+yourName = input ("Type your name -->")
+print("Nice to meet you, " + yourName)
+place = input("Where did you go-->")
+movie = input("What did you watch?-->")
+book = input("What did you read?-->")
+funfact = input("Give a fun fact about your break.-->")
+print("Mr. Manning went to " + place + " and watched " + movie + " and read " + book + " and had this fun fact " + funfact)
+
+
+
---
+++ createVariablesHW.py
@@ -0,0 +1,10 @@
+# I need a few variables
+# I need to print output
+Integer = 12
+FloatInteger = 45.6
+Integer = input ("Type a number here")
+FloatInteger = input("Type a number with a decimal here")
+# I have my variables and string variables. Now I need to work on printing
+print("the integer is " + Integer + " and the float integer is " + FloatInteger)
+
+
