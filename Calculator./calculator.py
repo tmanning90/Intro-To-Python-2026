89f48f2ce3831a2d13b29e1033176a30f7ca8f58
tmanning90 <tmanning@carolinaday.org>
2026-01-09 16:38:30 +0000

    try 2 with calculator

---
+++ Calculator./calculator.py
@@ -0,0 +1,15 @@
+a = float(input("type a number here-->"))
+b = float(input("type a different number here-->"))
+
+sum=a+b
+difference=a-b
+product=a*b
+quotient=a/b
+# I need if else statements with the operations
+
+Answer = (input ("What would you like me to do? add, subtract, multiply, or divide?-->"))
+
+print(sum)
+print(difference)
+print(product)
+print(quotient)
