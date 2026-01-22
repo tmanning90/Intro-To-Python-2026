66432fff60f9b4bb94709c91a446058d808b7e9a
tmanning90 <tmanning@carolinaday.org>
2026-01-22 16:37:55 +0000

    updated code

--- Gameshow.py
+++ Gameshow.py
@@ -4,23 +4,34 @@
 #We reveal if the contestant the right door
 import random
 
-winningDoor = random,randint(1,3) #pcks a random number 1-3
-contestantChoice = random,randint(1,3)
+def Winners():
+
+  winningDoor = random.randint(1,3) #pcks a random number 1-3
+  contestantChoice = random.randint(1,3)
 
 #print("Winner: " + str(winningDoor) + "; contestantChoice: " + str(contestantChoice))
 
-revealDoor = random,randint(1,3)
-while revealDoor == winningDoor or revealDoor == contestantChoice:
-  
-print("Winner: " + str(winningDoor) + "; contestantChoice: " + str(contestantChoice) + "; Host Opens Door #" + str(revealDoor))
+  revealDoor = random.randint(1,3)
+  while revealDoor == winningDoor or revealDoor == contestantChoice:
+   revealDoor = random.randint(1,3)
+#print("Winner: " + str(winningDoor) + "; contestantChoice: " + str(contestantChoice) + "; Host Opens Door #" + str(revealDoor))
 
-contestantFinal = random,randint(1,3)
-while contestFinal == constestandChoice or revealDoor:
-    contestFinal = random,randint(1,3)
+  contestantFinal = random.randint(1,3)
+  while contestantFinal == contestantChoice or contestantFinal == revealDoor:
+    contestantFinal = random.randint(1,3)
 
-if constestFinal == winningDoor
-  print("The contestant wins a brand new car!")
-else:
-  print("No winners today.")
+  if contestantFinal == winningDoor:
+    print("The contestant wins a brand new car!")
+  else:
+    print("No winners today.")
+
+Winners = 0
+for i in range(1000):
+  win=Winners()
+  if win:
+    Winners +=1
+print("Winners are " + str(Winners))
+
+          
   
       
