e6ef111b0fd0e60e11814a068ebeb87eed273afb
tmanning90 <tmanning@carolinaday.org>
2026-02-06 14:24:10 +0000

    practice

--- Calculator./Algorithms Feb 6.py
+++ Calculator./Algorithms Feb 6.py
@@ -16,4 +16,14 @@ def search(inputList, target, leftBound, rightBound):
     # Target is larger: search right half
     else:
         return search(inputList, target, mid + 1, rightBound)
-        
+
+
+
+myList = [0,3,4,9]
+print(search(myList, 9, 0, len(myList)))
+print(search(myList, 2, 0, len(myList)))
+
+newList = [1,25,3,14,5,64,7,8]
+print(search(newList, 2, 0, len(newList)))
+print(search(newList, 8, 0, len(newList)))
+            
